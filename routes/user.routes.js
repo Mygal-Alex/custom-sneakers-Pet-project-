1f0d7d1994fbd8997b035f15a8716a20194e6876
Mygal-Alex <magalalecs2003@gmail.com>
2023-04-14 15:54:11 +0300

    api fix

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,7 +4,7 @@ const UserController = require('../controller/user.controller')
 const userController = require('../controller/user.controller')
 
 router.post('/user', userController.createUser)
-router.get('/user', userController.getUser)
+router.get('/user', userController.getUsers)
 router.get('/user/:id', userController.getOneUser)
 router.put('/user', userController.updateUser)
 router.delete('/user/:id', userController.deleteUser)
