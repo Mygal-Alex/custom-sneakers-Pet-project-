8ff8f71bda7083d896ccefd79a39992ac814ebff
Mygal-Alex <magalalecs2003@gmail.com>
2023-04-18 00:37:36 +0300

    CRUD user

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -15,6 +15,12 @@ class UserController {
         const user = await db.query('SELECT * FROM persone where id = $1', [id])
         res.json(user.rows[0])
     }
+    // async getOneUserByAccount(req, res){
+    //         const nickname = req.params.nickname
+    //         const password_ = req.params.password_
+    //         const user = await db.query('SELECT * FROM persone where nickname = $1, password_ = $2', [nickname, password_])
+    //         res.json(user.rows[0])
+    //     }
     async updateUser(req, res) {
         const {email, nickname, phone, password_, name_, surname, id } = req.body
         const user = await db.query('UPDATE persone set email = $1, nickname = $2, phone = $3, password_ = $4, name_ = $5, surname = $6, isAdmin=$7 id = $8 RETURNING *',
